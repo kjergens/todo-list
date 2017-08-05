5be8c4103b727c65e24a088464fc9db89c34cbf4
Katie Jergens <katie.jergens@gmail.com>
2017-08-05 16:14:02 -0400

    firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import fire from './fire';
 import octo from './octo.svg';
 import Button from 'react-bootstrap/lib/Button';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
@@ -59,6 +60,7 @@ class App extends Component {
         </div>
         </div>
         <div className="container">
+        <ListOfStuff/>
           {this.state.alert}
           <InputBox addTask={this.addTask} />
           <TaskList taskList={this.state.taskList} deleteTask={this.deleteTask} />
@@ -162,6 +164,43 @@ class AlertBox extends Component {
   }
 }
 
+class ListOfStuff extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { messages: [] }; // <- set up react state
+  }
+
+  componentWillMount(){
+    /* Create reference to messages in Firebase Database */
+    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
+    messagesRef.on('child_added', snapshot => {
+      /* Update React state when message is added at Firebase Database */
+      let message = { text: snapshot.val(), id: snapshot.key };
+      this.setState({ messages: [message].concat(this.state.messages) });
+    })
+  }
+
+  addMessage(e){
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    /* Send the message to Firebase */
+    fire.database().ref('messages').push( this.inputEl.value );
+    this.inputEl.value = ''; // <- clear the input
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.addMessage.bind(this)}>
+        <input type="text" ref={ el => this.inputEl = el }/>
+        <input type="submit"/>
+        <ul>
+          { /* Render the list of messages */
+            this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
+          }
+        </ul>
+      </form>
+    );
+  }
+}
 
 
 export default App;
---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCabBvLbLkpoEnXPz26pof6RgFeHFxaIWc",
+  authDomain: "todo-list-afe54.firebaseapp.com",
+  databaseURL: "https://todo-list-afe54.firebaseio.com",
+  projectId: "todo-list-afe54",
+  storageBucket: "",
+  messagingSenderId: "814241752617"
+};
+firebase.initializeApp(config);
